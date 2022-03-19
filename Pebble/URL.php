c86d84b92e5206d53b3e716047d686b28d1eac24
Dennis Iversen <dennis.iversen@gmail.com>
2022-03-19 08:57:31 +0100

    method for getting part of url path

--- Pebble/URL.php
+++ Pebble/URL.php
@@ -24,12 +24,32 @@ class URL
     }
 
     /**
-     * Get a variable from query variable $_GET. If the query part is not set thenthe method returns null
+     * Get a variable from $_GET. If the query part is not set thenthe method returns null
      */
     public static function getQueryPart(string $str)
     {
         if (isset($_GET[$str])) {
             return $_GET[$str];
         }
+    }
+
+    /**
+     * Get a elem from url path: /some/path
+     */
+    public static function getUrlPath(int $num_elem): string
+    {
+        $route = $_SERVER['REQUEST_URI'];
+
+        $route = strtok($route, '?');
+        $url_parts = explode('/', $route);
+        $url_parts_filtered = [];
+        foreach ($url_parts as $url_part) {
+            if ($url_part) {
+                $url_parts_filtered[] = $url_part;
+            }
+        }
+
+        $part = $url_parts_filtered[$num_elem] ?? null;
+        return $part;
     }
 }
