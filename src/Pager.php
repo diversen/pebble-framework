82ff4668751328d2b956ce7122904bc0b89fa1be
Dennis Iversen <dennis.iversen@gmail.com>
2022-04-02 10:53:49 +0200

    clean up.

--- src/Pager.php
+++ src/Pager.php
@@ -4,13 +4,9 @@ namespace Pebble;
 
 use Pebble\URL;
 
-
 class Pager
 {
 
-    /**
-     * Current page in the set. 0 is the first page.
-     */
     public int $page;
     public int $limit;
     public int $offset;
@@ -22,20 +18,18 @@ class Pager
     public int $num_pages;
 
     private string $query_part;
-    private bool $zero_index;
 
     /**
-     * Get pager that can be used for pagination and setting offset, limit in DB queries
+     * Get a pager that can be used for pagination and setting offset and limit in DB queries
+     * 
      * @param int $total total items in the set
      * @param int $limit results per page
-     * @param string $query_art e.g. /index?page=1 where 'page' acts like the query part
-     * @param bool $zero_index if trye then the first 'page' is 0 - if false then the first page is 1
+     * @param string $query_part e.g. /index?page=1 where 'page' acts like the query part
      */
-    public function __construct(int $total, int $limit, string $query_part = 'page', bool $zero_index = true)
+    public function __construct(int $total, int $limit, string $query_part = 'page')
     {
 
         $this->query_part = $query_part;
-        $this->zero_index = $zero_index;
 
         $data = $this->getData($total, $limit);
 
@@ -53,25 +47,17 @@ class Pager
     private function getFrom() {
 
         if (!URL::getQueryPart($this->query_part)) {
-            if ($this->zero_index) {
-                $from = 0;
-            } else {
-                $from = 1;
-            }
+            $from = 1;
         } else {
             $from = (int)URL::getQueryPart($this->query_part);
         }
+
         return $from;
-            
     }
 
     private function getOffset($from, $limit) {
-        if($this->zero_index) {
-            $offset = $from * $limit;
-        } else {
-            $offset = ($from - 1) * $limit;
-        }
 
+        $offset = ($from - 1) * $limit;
         return $offset;
 
     }
