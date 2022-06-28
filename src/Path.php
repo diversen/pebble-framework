68e3bf259f47caa13af2d69d0a858cf82227f477
Dennis Iversen <dennis.iversen@gmail.com>
2022-06-28 21:17:59 +0200

    Also guess base path correct when pebble is not a dependency

--- src/Path.php
+++ src/Path.php
@@ -9,6 +9,14 @@ class Path
      */
     public static function getBasePath()
     {
+        $dir = dirname(__DIR__);
+
+        // If not installed as dependency
+        if (file_exists($dir . '/vendor')) {
+            return $dir;
+        }
+
+        // If installed as dependency
         return dirname(dirname(dirname(dirname(__dir__))));
     }
 }
