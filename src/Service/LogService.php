6b6b73b26ba2dee5b2bc0b164939e0d4fee89bd4
Dennis Iversen <dennis.iversen@gmail.com>
2022-07-02 11:17:23 +0200

    allow Logger to be read from config/Log.php

--- src/Service/LogService.php
+++ src/Service/LogService.php
@@ -11,6 +11,12 @@ use Pebble\Service\ConfigService;
 
 class LogService
 {
+
+    private $debug_level = Logger::DEBUG;
+    public function __construct() {
+        
+    }
+    
     /**
      * @var Monolog\Logger
      */
@@ -29,14 +35,40 @@ class LogService
             return self::$log;
         }
 
-        $base_path = Path::getBasePath();
-        $debug_level = (new ConfigService())->getConfig()->get('App.debug_level');
-        if (!$debug_level) {
-            $debug_level = Logger::DEBUG;
+        // Get log from config
+        self::$log = $this->getLogFromConfig();
+        if (self::$log) {
+            return self::$log;
         }
 
-        self::$log = new Logger('base');
-        self::$log->pushHandler(new StreamHandler($base_path . '/logs/main.log', $debug_level));
+        // Get default log
+        self::$log = $this->getDefaultLogger();
         return self::$log;
+    }
+
+    /**
+     * Get a log instance from `config/Log.php`. 'logger' key 
+     */
+    private function getLogFromConfig() {
+        $config = (new ConfigService())->getConfig();
+        if ($config->get('Log.logger')) {
+            return $config->get('Log.logger');
+        }
+    }
+
+    /**
+     * The default logger logs to the file logs/main.log
+     */
+    private function getDefaultLogger() {
+
+        $config = (new ConfigService())->getConfig();
+        if ($config->get('Log.level')) {
+            $this->debug_level = $config->get('Log.level');
+        }
+
+        $logger = new Logger('base');
+        $base_path = Path::getBasePath();
+        $logger->pushHandler(new StreamHandler($base_path . '/logs/main.log', $this->debug_level));
+        return $logger;
     }
 }
