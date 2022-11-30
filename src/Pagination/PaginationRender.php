3bdbb09258fbfb3682a2bc091c701e71afd2722e
Dennis Iversen <dennis.iversen@gmail.com>
2022-11-30 13:32:54 +0100

    add some pagination helpers

---
+++ src/Pagination/PaginationRender.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pebble\Pagination;
+
+/**
+ * Class that renders the pagination links in HTML using the JasonGrimes/Paginator library.
+ * Mostly of how to render the \JasonGrimes\Paginator
+ */
+class PaginationRender
+{
+    public function render(\JasonGrimes\Paginator $paginator) { ?>
+
+        <div class="pagination"><?php
+
+        $num_pages = count($paginator->getPages());
+
+        if ($num_pages):
+
+            if ($paginator->getPrevUrl()): ?>
+                <a class='pagination-links' href='<?=$paginator->getPrevUrl()?>'>&lt;&lt;</a>
+            <?php else: ?>
+                <span class='pagination-links'>&lt;&lt;</span><?php
+            endif;
+
+            foreach ($paginator->getPages() as $page):
+
+                $css_class = 'pagination-links';
+                if ($page['url']):
+                    if ($page['isCurrent']):
+                        $css_class = 'pagination-links pagination-current';
+                    endif; ?>
+                    <a class="<?=$css_class?>" href="<?=$page['url']; ?>"><?=$page['num']; ?></a><?php
+                else: ?>
+                    <span class="<?=$css_class?>" disabled><?php echo $page['num']; ?></span>
+                <?php endif; ?>
+            <?php
+
+            endforeach;
+
+            if ($paginator->getNextUrl()):?>
+                <a class='pagination-links' href='<?=$paginator->getNextUrl()?>'>&gt;&gt;</a><?php
+            else: ?>
+                <span class='pagination-links'>&gt;&gt;</span><?php
+            endif; ?>        
+            <?php
+        endif; ?>
+        </div><?php
+    }
+}
--- src/Pagination/PaginationUtils.php
+++ src/Pagination/PaginationUtils.php
@@ -1,11 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pebble\Pagination;
 
 use InvalidArgumentException;
 use Exception;
 use Pebble\URL;
+use JasonGrimes\Paginator;
 
+/**
+ * Class that helps render pagination with order_by, from, and saves the order_by and from in session.
+ */
 class PaginationUtils
 {
     /**
@@ -224,5 +230,27 @@ class PaginationUtils
         } else {
             return "↓";
         }
+    }
+
+    /**
+     * Get JasonGrimes/Paginator and with order by saved to session 
+     */
+    public static function getPaginator(
+        int $total_items,
+        int $items_per_page,
+        int $current_page,
+        string $url,
+        array $default_order = [],
+        int $max_pages = 10,
+        string $session_key = null,
+    ) {
+
+        $pagination_utils = new PaginationUtils($default_order, $session_key);
+        $url_pattern = $pagination_utils->getPaginationURLPattern($url);
+
+        $paginator = new Paginator($total_items, $items_per_page, $current_page, $url_pattern);
+        $paginator->setMaxPagesToShow($max_pages);
+
+        return $paginator;
     }
 }
