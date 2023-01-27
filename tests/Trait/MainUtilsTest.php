483159243c0b5f8a69361bbdd0546a83c3141e97
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-27 10:41:04 +0100

    Mv AppBaseTest -> MainUtilsTest

--- tests/Trait/MainUtilsTest.php
+++ tests/Trait/MainUtilsTest.php
@@ -1,28 +1,32 @@
 <?php
 
-use Pebble\App\AppBase;
+use Pebble\Trait\MainUtils;
 use PHPUnit\Framework\TestCase;
 use function Safe\get_include_path;
 
-final class AppBaseTest extends TestCase
+final class MainUtilsTest extends TestCase
 {
     public function test_AppBase(): void
     {
+
+        $main_utls = new class {
+            use MainUtils;
+        };
         $include_path_original = get_include_path();
-        $app_base = new AppBase();
-        $app_base->addIncludePath('/tmp');
+        // $app_base = new AppBase();
+        $main_utls->addIncludePath('/tmp');
 
         $include_path = get_include_path();
         $this->assertStringContainsString('/tmp', $include_path);
         set_include_path($include_path_original);
 
         // This will only work if the repo is checked out as pebble-framework
-        $app_base->addBaseToIncudePath();
+        $main_utls->addBaseToIncudePath();
         $include_path = get_include_path();
         $this->assertStringContainsString('pebble-framework', $include_path);
         set_include_path($include_path_original);
 
-        $res = $app_base->setErrorHandler();
+        $res = $main_utls->setErrorHandler();
         if ($res) {
             // Old error handler PHPUnit\Util\ErrorHandler
             $this->assertIsCallable($res);
