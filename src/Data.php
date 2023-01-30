17daf91290fb69bd8533e7a1e0f261e8576f6197
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-30 10:56:05 +0100

    add ArrayData methods

--- src/Data.php
+++ src/Data.php
@@ -7,6 +7,10 @@ namespace Pebble;
 /**
  * A class to store data on a single script execution
  * E.g. for storing data for display in a template
+ * 
+ * setData() and getData() is used for storing single values
+ * setArrayData() and getArrayData() is used for storing arrays of data
+ * 
  */
 class Data {
 
@@ -15,11 +19,27 @@ class Data {
      */
     public array $data = [];
 
-    public function setData (string $key, mixed $value): void {
+    public function setData (string $key, mixed $value):void {
         $this->data[$key] = $value;
     }
 
     public function getData(string $key): mixed {
         return $this->data[$key] ?? null;
+    }
+
+    public function hasData(string $key): bool {
+        return isset($this->data[$key]);
+    }
+
+    public function setArrayData(string $key, array $value): void {
+        $this->data[$key][] = $value;
+    }
+
+    public function getArrayData(string $key): array {
+        return $this->data[$key] ?? [];
+    }
+
+    public function hasArrayData(string $key): bool {
+        return isset($this->data[$key]) && is_array($this->data[$key]);
     }
 }
